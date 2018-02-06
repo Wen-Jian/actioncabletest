ebbd196d49d89c2fd98d121cac2979f1f6b5abce
Wen <genning7@gmail.com>
2018-02-06 17:15:45 +0800

    first commit

---
+++ app/controllers/chatrooms_controller.rb
@@ -0,0 +1,48 @@
+class ChatroomsController < ApplicationController
+
+	def index
+
+		@chatrooms = Chatroom.all
+		
+	end
+
+	def show
+		
+		
+		
+	end
+
+	def new
+
+		@chatroom = Chatroom.new
+		
+	end
+
+	def create
+
+		@chatroom = current_user.chatrooms.build(chatroom_params)
+		if @chatroom.save
+
+			flash[:success] = 'Chat room build successfully'
+			redirect_to chatrooms_path
+
+		else
+
+			render 'new'
+
+		end
+
+		private 
+
+		def chatroom_params
+
+			params.require(:chatroom).permit(:title)
+			
+		end
+		
+	end
+
+
+
+
+end
